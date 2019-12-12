16a13a69ab988bbc4168f142bfbed674522c2b4d
Regular IT-cat <c21foodog@gmail.com>
2019-12-13 06:02:42 +0700

    migrations, tags

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ class Configuration(object):
     DEBUG = True
 
     # SQLAlchemy configuration
+    # pls don't hack me(((99(((
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1@localhost/flask_test1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
